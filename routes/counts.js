95f1bef7ed8e194ebc3d380bda4f5baa7a1aa519
Travis <travis@denouncetheclock.com>
2022-01-22 10:38:19 +1100

    Add basic postgres test

--- routes/counts.js
+++ routes/counts.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const {Pool} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  res.json({"chatDamage": 10, "promisesBroken": 2, "promisesKept": 0});
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+  // res.json({"chatDamage": 10, "promisesBroken": 2, "promisesKept": 0});
 });
 
 module.exports = router;
