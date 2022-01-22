e6cf7081b48af828ade289f7b2f56d546c9ca20b
Travis <travis@denouncetheclock.com>
2022-01-23 10:04:15 +1100

    Add stats route to just get constructed message

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 const countsRouter = require('./routes/counts');
 const getRouter = require('./routes/get');
 const addRouter = require('./routes/add');
+const statsRouter = require('./routes/stats');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/counts', countsRouter);
 app.use('/add', addRouter);
 app.use('/get', getRouter);
+app.use('/stats', statsRouter);
 // app.use('/users', usersRouter);
 
 // app.post('/add', async(req, res) => doAdd(req, res));
---
+++ routes/stats.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const {Pool} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const constructLines = (resultsArray) => {
+  return resultsArray.map(x => x.message.replace('{0}', x.amount));
+}
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM counts');
+    
+    const m = {
+      message: constructLines(result)
+    };
+
+    res.json(m);
+    // res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+  // res.json({"chatDamage": 10, "promisesBroken": 2, "promisesKept": 0});
+});
+
+module.exports = router;
