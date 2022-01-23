8228fd12998d418edb4a0ed5449df758f92443d6
Travis <travis@denouncetheclock.com>
2022-01-23 11:56:19 +1100

    Try fix connection error

--- sequalize.js
+++ sequalize.js
@@ -1,7 +1,12 @@
-const { Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-if(process.env.NODE_ENV == "production") {
-  module.exports = new Sequelize(process.env.DATABASE_URL, {ssl: false});
+if (process.env.NODE_ENV == "production") {
+  module.exports = new Sequelize(process.env.DATABASE_URL, {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    }
+  });
 } else {
   console.log('dev')
   module.exports = new Sequelize('sqlite::memory:');
